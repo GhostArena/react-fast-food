c523e71cda1a2e8c35889668957d36d491b2f42f
Gabriel Omane - Yeboah <gabrielomane@outlook.com>
2023-09-24 12:26:30 +0000

    Finished styling the app

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Form, redirect } from "react-router-dom";
+import Button from "../../ui/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -73,10 +74,10 @@ function CreateOrder() {
         </div>
 
         <div>
-          <button className="inline-block px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2">
+          <Button disabled="" type="primary">
             {/* {isSubmitting ? "Placing rder..." : "Order now"} */}
             Order now
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
---
+++ src/ui/Button.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Button = ({ children, disabled, to, type }) => {
+  const base =
+    "inline-block font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:duration-300 focus:outline-none focus:ring disabled:cursor-not-allowed focus:ring-offset-2";
+  const styles = {
+    primary: base + "px-4 py-3 md:px-6 md:py-4",
+    small: base + "px-4 py-2 md:px-5 md:py-2.5 text-xs",
+  };
+  const className =
+    "inline-block px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:duration-300 focus:outline-none focus:ring disabled:cursor-not-allowed focus:ring-offset-2 sm:px-6 sm:py-4";
+  if (to) {
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
+  }
+
+  if (type === "small") {
+    return (
+      <button disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
+    );
+  }
+  return (
+    <button
+      disabled={disabled}
+      className="inline-block px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 disabled:cursor-not-allowed focus:ring-offset-2 sm:px-6 sm:py-4"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
